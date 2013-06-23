6f29afb5e35f1260a25ca5cd45a11d04911ae4ad
Juliano Suzuki <juliano@getninjas.com.br>
2013-06-22 22:54:00 -0300

    Feito outro exercicio da pagina 1

--- exercicios_pag_1.rb
+++ exercicios_pag_1.rb
@@ -6,3 +6,13 @@ puts "Minutos em uma década"
 minutos_dia = 60 * 24
 minutos_ano = minutos_dia * 365
 puts minutos_decada = minutos_ano * 10
+
+puts "Segundos da minha idade"
+segundos_hora = 60 * 60
+segundos_dia = segundos_hora * 24
+segundos_ano = segundos_dia * 365
+puts segundos_minha_idade = segundos_ano * 24
+
+puts "Idade de 1164 milhões de segundos"
+segundos_ano = segundos_dia * 365
+puts segundos_1164milhoes =  1164000000 / segundos_ano
